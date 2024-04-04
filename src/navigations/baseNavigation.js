c201787e529e482c45a262211d48f638b8de467b
mubashircha19 <mubashircha19@gmail.com>
2024-04-04 21:13:59 +0500

    all file chnages

--- src/navigations/baseNavigation.js
+++ src/navigations/baseNavigation.js
@@ -2,21 +2,22 @@ import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import Signup from "../screens/signup";
 import Home from "../screens/Home";
+import Login from "../screens/login";
+import Splash from "../screens/splash";
 
 const Stack = createNativeStackNavigator();
 
 function BaseNavigation() {
   return (
     <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen
-          name={"Signup"}
-          component={Signup}
-          options={{
-            headerShown: false,
-          }}
-        />
-
+      <Stack.Navigator
+        screenOptions={{
+          headerShown: false,
+        }}
+      >
+        <Stack.Screen name="Splash" component={Splash} />
+        <Stack.Screen name="Login" component={Login} />
+        <Stack.Screen name={"Signup"} component={Signup} />
         <Stack.Screen name="Home" component={Home} />
       </Stack.Navigator>
     </NavigationContainer>
